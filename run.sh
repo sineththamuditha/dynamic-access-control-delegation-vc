3f9c61a527be1e838d69c6c338cd1286609e46ae
sineththamuditha <sineththamuditha@gmail.com>
2025-02-20 22:45:18 +0530

    Change run.sh to new configuration

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
-/Users/sineththamuditha/Documents/dynamic-access-control-delegation-vc/hyperldger/von-network/manage start
+/Users/sineththamuditha/Documents/dynamic-access-control-delegation-vc/hyperledger/von-network/manage start
 
-./hyperldger/aries-cloudagent-python/scripts/run_docker start \
+./hyperledger/acapy/scripts/run_docker start \
     --inbound-transport http 0.0.0.0 8000 \
     --outbound-transport http \
     --genesis-url http://host.docker.internal:9000/genesis \
     --endpoint http://localhost:8020 \
     --admin-insecure-mode \
-    --admin 0.0.0.0 9021 
+    --admin 0.0.0.0 9021 \
+    --wallet-name local_net_trustee_wallet \
+    --wallet-key EX8r5H1Vpot2i6bJSrXA/CO87TwT2Y1pL5H8tjluPbTCEbRUEgOF4K2NqbyayXHw \
+    --auto-provision \
+    --log-level info
 
-/Users/sineththamuditha/Documents/dynamic-access-control-delegation-vc/hyperldger/von-network/manage stop
+/Users/sineththamuditha/Documents/dynamic-access-control-delegation-vc/hyperledger/von-network/manage stop
